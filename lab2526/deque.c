eddf53c700bbd8ac8c9f7aa79401fcfa4bbc71b3
Brinckley <57827932+Brinckley@users.noreply.github.com>
2020-05-17 00:13:06 +0300

    Update deque.c

--- lab2526/deque.c
+++ lab2526/deque.c
@@ -16,15 +16,12 @@ int deque_size(Deque *D) {
     return D -> last;
 }
 
-void deque_push_front(Deque *D) {
+
+void deque_push_front(Deque *D, int value) {
     if (D -> last == SIZE) {
         printf("\nДек заполнен\n\n");
         return;
     }
-    int value;
-    printf("Введите число: ");
-    scanf("%d", &value);
-
     for (int i = D -> last; i > 0; i--) {
         D -> data[i].value = D -> data[i-1].value;
     }
@@ -33,14 +30,11 @@ void deque_push_front(Deque *D) {
     printf("Элемент добавлен\n");
 }
 
-void deque_push_back(Deque *D) {
+void deque_push_back(Deque *D, int value) {
     if (D -> last == SIZE) {
         printf("\nДек заполнен\n\n");
         return;
     }
-    int value;
-    printf("Введите число: ");
-    scanf("%d", &value);
     D -> data[D -> last++].value = value;
     printf("Элемент добавлен\n");
 }
@@ -66,17 +60,25 @@ void deque_print(Deque *D) {
 }
 
 void deque_sort_bubble(Deque *D){
+    Deque* tmp;
+    deque_create(tmp);
     if(deque_empty(D) != 1){
         for(int i = 0; i < deque_size(D); ++i) {
-            for (int j = 1; j < deque_size(D); ++j)
-            {
-                if(D -> data[j - 1].value > D -> data[j].value) {
-                    int tmp = D -> data[j - 1].value;
-                    D -> data[j - 1].value = D -> data[j].value;
-                    D -> data[j].value = tmp;
-                }
+            if(tmp -> last == 0) {
+                deque_push_back(tmp, D -> data[0].value);
+                deque_pop_front(D);
+                continue;
+            }
+            if(tmp -> data[tmp -> last - 1].value < D -> data[0].value) {
+                deque_push_back(tmp, D -> data[0].value);
+                deque_pop_front(D);
+            } else {
+                int mid = tmp -> data[tmp -> last - 1].value;
+                tmp -> data[tmp -> last - 1].value = D -> data[0].value;
+                deque_push_back(tmp, mid);
+                deque_pop_front(D);
             }
         }
+        D = tmp;
     }
-    
 }
