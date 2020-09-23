da874176d6633e791e688ffd41509d27d5a2a897
Brinckley <57827932+Brinckley@users.noreply.github.com>
2020-09-23 13:16:44 +0300

    release

---
+++ 2ndCourseLabs/Lab_3/Lab_3/F.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_3
+{
+    class F
+    {
+        protected int number = 5;
+
+        public F() {
+            Console.WriteLine("Constructor of F class");
+            number = 0;
+        }
+
+        public F(int number) {
+            this.number = number;
+        }
+
+        public int f { get { return number; } }
+
+        public virtual int getterNum() {
+            return number;
+        }
+
+    }
+
+    class D : F
+    {
+        public D() {
+            Console.WriteLine("Constructor of F class");
+            number++;
+        }
+
+        public D(int arg) {
+            number = arg;
+        }
+
+        public override int getterNum() {
+            return 2 * number;
+        }
+    }
+
+    class G : D
+    {
+        private int number_G;
+
+        public G(int arg1, int arg2) : base(arg1)
+        {
+            number_G = arg2;
+        }
+
+        public G() {
+            Console.WriteLine("Constructor of F class");
+            number++;
+        }
+
+        public override int getterNum()
+        {
+            return 3 * number;
+        }
+    }
+}
---
+++ 2ndCourseLabs/Lab_3/Lab_3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            F f_obj = new F();
+            Console.WriteLine("Number from F object: " + f_obj.getterNum());
+            Console.WriteLine();
+
+            f_obj = new D();
+            Console.WriteLine("Number from D object: " + f_obj.getterNum());
+            Console.WriteLine();
+
+            f_obj = new G();
+            Console.WriteLine("Number from G object: " + f_obj.getterNum());
+            Console.WriteLine();
+
+
+            f_obj = new D();
+
+
+            Console.ReadKey();
+        }
+    }
+}
