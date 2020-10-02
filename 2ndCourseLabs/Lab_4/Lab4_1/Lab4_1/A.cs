665d6b192f560018493b03dfb8f95ae442a9e037
Brinckley <57827932+Brinckley@users.noreply.github.com>
2020-10-02 22:50:06 +0300

    release

---
+++ 2ndCourseLabs/Lab_4/Lab4_1/Lab4_1/A.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4_1
+{
+    
+    //expansion
+    class A
+    {
+        protected int a_num;
+
+        public int A_num { get; set; }
+
+        public A() {
+            a_num = 2;
+        }
+
+        public virtual void writer() {
+            Console.WriteLine("In A superclass method. Value: " + a_num);
+        }
+    }
+    
+    class C : A
+    {
+        protected int c_num;
+
+        public int C_num { get; set; }
+
+        public C()
+        {
+            c_num = 222;
+        }
+
+        public new void writer()
+        {
+            Console.WriteLine("In С expansion method. Values: " + a_num + ", " + c_num);
+        }
+
+        public void writer_C()
+        {
+            Console.WriteLine("In С own method. Values: " + a_num + ", " + c_num);
+        }
+    }
+
+
+    //specification
+    interface C_Interface
+    {
+        void writer();
+    }
+
+    class D_class : C_Interface
+    {
+        protected int d_num;
+
+        public int D_num { get; set; }
+
+        public D_class()
+        {
+            d_num = 22;
+        }
+        
+        public void writer()
+        {
+            Console.WriteLine("In D specification method. Value: " + d_num);
+        }
+    }
+
+    
+    //specialization
+    class B_class {
+        protected int b_class_num;
+
+        public B_class() {
+            b_class_num = 2222;
+        }
+        
+        public virtual void writer() {
+            Console.WriteLine("Parent specialization function");
+        }
+    }
+    
+    class F_class : B_class {
+
+        protected int f_class_num;
+
+        public F_class() {
+            f_class_num = 22222;
+        }
+        
+        public override void writer() {
+            Console.WriteLine("In F_class specification method.Values: " + f_class_num + ", " + b_class_num);
+        }
+    }
+
+
+    //constructor, combination
+    interface A_interface
+    {
+        void printing1();
+        int numberReturner();
+    }
+
+    interface B_interface
+    {
+        void printing2();
+        string stringReturner();
+    }
+
+    class J_class : A_interface, B_interface
+    {
+        private int j_field;
+        private string j_string;
+
+        public J_class() {
+            j_field = 22222;
+            j_string = "222222";
+        }
+
+        public int numberReturner()
+        {
+            return j_field;
+        }
+
+        public void printing1()
+        {
+            Console.WriteLine("String and number = " + j_field + j_string);
+        }
+
+        public void printing2()
+        {
+            Console.WriteLine("String & number = " + j_field + j_string);
+        }
+
+        public string stringReturner()
+        {
+            return j_string;
+        }
+    }
+
+}
---
+++ 2ndCourseLabs/Lab_4/Lab4_2/Lab4_2/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+
+namespace Lab4_2
+{
+    interface A
+    {
+        void writer();
+    }
+
+    class J : A
+    {
+        private int j_field;
+
+        public int j_Getter { get; set; }
+
+        public J() { j_field = 2; }
+
+        public void writer() {
+            Console.WriteLine("Value of J: " + j_field);
+        }
+    }
+
+
+    abstract class B : A
+    {
+        public abstract void writer();
+
+        public abstract void writer_B();
+    }
+
+    abstract class C : A
+    {
+        public abstract void writer();
+
+        public abstract void writer_C();
+    }
+
+
+    class D : B
+    {
+        int d_field;
+
+        public D() {
+            d_field = 22;
+        }
+
+        public D(int d_field)
+        {
+            this.d_field = d_field;
+        }
+
+        public override void writer()
+        {
+            Console.WriteLine("Value of this field: " + d_field);
+        }
+
+        public override void writer_B()
+        {
+            Console.WriteLine("We've come from D abstract class");
+        }
+    }
+
+    class F : C
+    {
+        int f_field;
+
+        public F()
+        {
+            f_field = 222;
+        }
+
+        public F(int f_field)
+        {
+            this.f_field = f_field;
+        }
+
+        public override void writer()
+        {
+            Console.WriteLine("Value of this field: " + f_field);
+        }
+
+        public override void writer_C()
+        {
+            Console.WriteLine("We've come from C abstract class");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            J j_obj = new J();
+            j_obj.writer();
+            Console.WriteLine();
+            
+            D d_obj = new D();
+            d_obj.writer();
+            d_obj.writer_B();
+            Console.WriteLine();
+
+            F f_obj = new F();
+            f_obj.writer();
+            f_obj.writer_C();
+
+            Console.ReadKey();
+        }
+    }
+}
