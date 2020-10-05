e9afa1ba2b2a2be6c7ddb99ff777d3028fcb2e6b
Brinckley <57827932+Brinckley@users.noreply.github.com>
2020-10-05 23:44:36 +0300

    release

---
+++ 2ndCourseLabs/Lab_6/Lab6_1/Lab6_1/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+
+namespace Lab6_1
+{
+
+    class A {
+        public A() { }
+        public A(B b, C c, J j) { this.b = b; this.c = c; this.j = j; }
+
+        public void aHashCode() { Console.WriteLine("A class hash:   " + this.GetHashCode()); }
+
+        B bField;
+        public B b { get { return bField; } set { if (value != null) { bField = value; } } }
+
+        C cField;
+        public C c { get { return cField; } set { if (value != null) { cField = value; } } }
+
+        J jField;
+        public J j { get { return jField; } set { if (value != null) { jField = value; } } }
+    }
+
+    class B {
+        public B() { }
+        public B(D d) { this.d = d; }
+
+        public void bHashCode() { Console.WriteLine("B class hash:   " + this.GetHashCode()); }
+
+        D dField;
+        public D d { get { return dField; } set { if (value != null) { dField = value; } } }
+    }
+
+    class C
+    {
+        public C() { }
+        public C(F f) { this.f = f; }
+
+        public void cHashCode() { Console.WriteLine("C class hash:   " + this.GetHashCode()); }
+
+        F fField;
+        public F f { get { return fField; } set { if (value != null) { fField = value; } } }
+    }
+
+    class J
+    {
+        public J() { }
+        public J(A a) { this.a = a; }
+
+        public void jHashCode() { Console.WriteLine("J class hash:   " + this.GetHashCode()); }
+
+        A aField;
+        public A a { get { return aField; } set { if (value != null) { aField = value; } } }
+    }
+
+    class D {
+        public D() { }
+        public D(A a) { this.a = a; }
+
+        public void dHashCode() { Console.WriteLine("D class hash:   " + this.GetHashCode()); }
+
+        A aField;
+        public A a { get { return aField; } set { if (value != null) { aField = value; } } }
+    }
+
+    class F {
+        public F() { }
+        public F(A a) { this.a = a; }
+
+        public void fHashCode() { Console.WriteLine("F class hash:   " + this.GetHashCode()); }
+
+        A aField;
+        public A a { get { return aField; } set { if (value != null) { aField = value; } } }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            F fObj = new F();
+            D dObj = new D();
+            J jObj = new J();
+            B bObj = new B(dObj);
+            C cObj = new C(fObj);
+            A aObj = new A(bObj, cObj, jObj);
+            fObj.a = aObj;
+            dObj.a = aObj;
+            jObj.a = aObj;
+
+            aObj.aHashCode();
+            aObj.b.d.a.aHashCode();
+            aObj.c.f.a.aHashCode();
+            aObj.j.a.j.a.aHashCode();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 2ndCourseLabs/Lab_6/Lab6_2/Lab6_2/Program.cs
@@ -0,0 +1,141 @@
+﻿using System;
+
+namespace Lab6_2
+{
+    class Program
+    {
+        class A {
+            int sizeB;
+            B[] bArray;
+            int lengthB;
+            
+            int sizeC;
+            C[] cArray;
+            int lengthC;
+            
+            int sizeJ;
+            J[] jArray;
+            int lengthJ;
+
+            public A() {
+                lengthB = 4; bArray = new B[sizeB];
+                lengthC = 4; cArray = new C[sizeC];
+                lengthJ = 4; jArray = new J[sizeJ];
+            }
+            public A(int lengthB, int lengthC, int lengthJ) {
+                this.lengthB = lengthB; bArray = new B[lengthB];
+                this.lengthC = lengthC; cArray = new C[lengthC];
+                this.lengthJ = lengthJ; jArray = new J[lengthJ];
+            }
+
+            public void AddB(B b) { if (sizeB < lengthB) { bArray[sizeB] = b; sizeB++; } }
+            public B GetB(int number) { if (number >= 0 && number < sizeB) { return bArray[number]; } return null; }
+
+            public void AddC(C c) { if (sizeC < lengthC) { cArray[sizeC] = c; sizeC++; } }
+            public C GetC(int number) { if (number >= 0 && number < sizeC) { return cArray[number]; } return null; }
+
+            public void AddJ(J j) { if (sizeJ < lengthJ) { jArray[sizeJ] = j; sizeJ++; } }
+            public J GetJ(int number) { if (number >= 0 && number < sizeJ) { return jArray[number]; } return null; }
+
+            public void HashCode() { Console.WriteLine("A class hash:  " + this.GetHashCode()); }
+        }
+
+        class B {
+            int size;
+            D[] dArray;
+            int length;
+
+            public B() { length = 4; dArray = new D[size]; }
+            public B(int length) { this.length = length; dArray = new D[length]; }
+
+            public void Add(D d) { if (size < length) { dArray[size] = d; size++; } }
+            public D Get(int number) { if (number >= 0 && number < size) { return dArray[number]; } return null; }
+
+            public void HashCode() { Console.WriteLine("B class hash:  " + this.GetHashCode()); }
+
+        }
+
+        class C {
+            int size;
+            F[] fArray;
+            int length;
+
+            public C() { length = 4; fArray = new F[size]; }
+            public C(int length) { this.length = length; fArray = new F[length]; }
+
+            public void Add(F f) { if (size < length) { fArray[size] = f; size++; } }
+            public F Get(int number) { if (number >= 0 && number < size) { return fArray[number]; } return null; }
+
+            public void HashCode() { Console.WriteLine("C class hash:  " + this.GetHashCode()); }
+
+        }
+
+        class J
+        {
+            A aObj;
+            public A a { get { return aObj; } set { if (value != null) { aObj = value; } } }
+
+            int JNum;
+            public int jNum { get { return JNum; } }
+
+            public J() { }
+            public J(A a, int JNum) { this.a = a; this.JNum = JNum; }
+
+            public void HashCode() { Console.WriteLine("J class hash:  " + this.GetHashCode()); }
+
+        }
+
+        class D {
+            A aObj;
+            public A a { get { return aObj; } set { if (value != null) { aObj = value; } } }
+
+            int DNum;
+            public int dNum { get { return DNum; } }
+
+            public D() { }
+            public D(A a, int DNum) { this.a = a; this.DNum = DNum; }
+
+            public void HashCode() { Console.WriteLine("D class hash:  " + this.GetHashCode()); }
+        }
+
+        class F {
+            A aObj;
+            public A a { get { return aObj; } set { if (value != null) { aObj = value; } } }
+
+            int FNum;
+            public int fNum { get { return FNum; } }
+
+            public F() { }
+            public F(A a, int FNum) { this.a = a; this.FNum = FNum; }
+
+            public void HashCode() { Console.WriteLine("F class hash:  " + this.GetHashCode()); }
+        }
+        static void Main(string[] args)
+        {
+            F fObj = new F();
+            D dObj = new D();
+            J jObj = new J();
+
+            C cObj = new C(4);
+            cObj.Add(fObj);
+            B bObj = new B(4);
+            bObj.Add(dObj);
+
+            A aObj = new A(4, 4, 4);
+            fObj.a = aObj;
+            dObj.a = aObj;
+            jObj.a = aObj;
+
+            aObj.AddB(bObj);
+            aObj.AddC(cObj);
+            aObj.AddJ(jObj);
+
+            aObj.HashCode();
+            aObj.GetB(0).Get(0).a.HashCode();
+            aObj.GetC(0).Get(0).a.HashCode();
+            aObj.GetJ(0).a.HashCode();
+
+            Console.ReadKey();
+        }
+    }
+}
