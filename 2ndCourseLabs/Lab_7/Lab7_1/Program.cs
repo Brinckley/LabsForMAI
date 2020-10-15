f81ddd5edc22b354e7defd1abd67bad17a280e91
Brinckley <57827932+Brinckley@users.noreply.github.com>
2020-10-15 14:32:05 +0300

    release

---
+++ 2ndCourseLabs/Lab_7/Lab7_1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Lab7_1
+{
+
+    class Server {
+        public Server() {}
+
+        public int serverReturner() {
+            return 1;
+        }
+    }
+
+    static class Utility {
+        public static int staticReturner() {
+            return 11;
+        }
+    }
+
+    class Client {
+        public Client() { }
+
+        public void serverWriter(Server server) { Console.WriteLine("Client calls server function: " + server.serverReturner()); }
+        public void staticReturner() { Console.WriteLine("Cleint calls utility: " + Utility.staticReturner()); }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Client client = new Client();
+            Server server = new Server();
+
+            client.serverWriter(server);
+            client.staticReturner();
+        }
+    }
+}
---
+++ 2ndCourseLabs/Lab_8/Lab8_1/Lab8_1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Lab8_1
+{
+
+    class OverRide<T> {
+
+        public T tmp { get; set; }
+
+        public OverRide(T tmp) {
+            this.tmp = tmp;
+        }
+
+        public void print() { Console.WriteLine("Printing value: " + tmp);
+            String type = "Value type: ";
+            if (tmp is int) {
+                type += "int";
+            } else if (tmp is string) {
+                type += "string";
+            } else if (tmp is bool) {
+                type += "bool";
+            } else { type += "unknown type"; }
+            Console.WriteLine(type);
+        }
+
+        public static void swap(ref T tmp1, ref T tmp2) {
+            T tmp3 = tmp1;
+            tmp1 = tmp2;
+            tmp2 = tmp3;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OverRide<int> value1_int = new OverRide<int>(1);
+            OverRide<int> value2_int = new OverRide<int>(2);
+            value1_int.print();
+            value2_int.print();
+            Console.WriteLine();
+
+            OverRide<OverRide<int>>.swap(ref value1_int, ref value2_int);
+            value1_int.print();
+            value2_int.print();
+
+            Console.ReadKey();
+        }
+    }
+}
