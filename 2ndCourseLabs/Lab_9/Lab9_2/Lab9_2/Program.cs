d33b70a0932ab69501a28fb5066569ceb4f90a15
Brinckley <57827932+Brinckley@users.noreply.github.com>
2020-10-28 18:34:34 +0300

    release

---
+++ 2ndCourseLabs/Lab_9/Lab9_2/Lab9_2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab9_2
+{
+    class Program
+    {
+        public static bool lambda(int x, int y) => x > y; 
+        
+        delegate bool PositiveCheck(int tmp1, int tmp2);
+
+        static void Main(string[] args) {
+            PositiveCheck positiveCheck = (x, y) => (x + y) > 0;
+            Console.WriteLine("Result of the check: " + positiveCheck(-77, 88));
+            Console.WriteLine();
+
+            Console.WriteLine("Just Lambda function result (for 2 and 9): " + lambda(2, 9));
+            Console.WriteLine();
+
+            List<int> list = new List<int>() { 0, -8, -9, 98, 55 };
+            Console.WriteLine("First positive number in list: " + list[list.FindIndex(x => x > 0)]);
+            Console.ReadKey();
+        }
+        
+        
+    }
+}
---
+++ 2ndCourseLabs/Lab_9/Lab9_3/Lab9_3/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+namespace Lab9_3
+{
+    public class Publisher
+    {
+        public Publisher() { }
+        public Publisher(int number) { this.number = number; }
+
+        private int number;
+        public int Number { get { return number; }
+            set {
+                if (changeEvent != null) { changeEvent("Field has been changed!", value); }
+                number = value;
+            }
+        }
+
+        public void changeValue(int Number) {
+            this.Number = Number;
+        }
+
+        public delegate void ChangeHandler(string tmp, int newValue);
+        public event ChangeHandler changeEvent;
+
+    }
+
+    public class Subscriber
+    {
+        private Publisher publisher = null;
+        private int num;
+
+        public Subscriber(Publisher publisher, int num) {
+            this.publisher = publisher;
+            this.num = num;
+            this.publisher.changeEvent += ChangeEventHandler;
+        }
+
+        public void ChangeFunctionCaller(int tmp) {
+            publisher.changeValue(tmp);
+        }
+
+        public void ChangeEventHandler(string msg, int newValue) {
+            Console.WriteLine(msg);
+            Console.WriteLine("New value for publishers field is " + newValue);
+            Console.WriteLine("From subscriber number " + num);
+        }
+    }
+
+    class AnotherSubscriber {
+        private Publisher publisher = null;
+        private int num;
+
+        public AnotherSubscriber(Publisher publisher, int num) {
+            this.publisher = publisher;
+            this.num = num;
+            this.publisher.changeEvent += ChangeEventHandler;
+        }
+
+        public void ChangeFunctionCaller(int tmp) {
+            publisher.changeValue(tmp);
+        }
+
+        public void ChangeEventHandler(string msg, int newValue) {
+            Console.WriteLine(msg);
+            Console.WriteLine("New value for publishers field is " + newValue);
+            Console.WriteLine("From another subscriber with number " + num);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Publisher publisher = new Publisher(55);
+            Subscriber subscriber = new Subscriber(publisher, 1);
+            AnotherSubscriber anotherSubscriber = new AnotherSubscriber(publisher, 2);
+
+            subscriber.ChangeFunctionCaller(66);
+            Console.WriteLine();
+            anotherSubscriber.ChangeFunctionCaller(78);
+
+            Console.ReadKey();
+        }
+    }
+}
