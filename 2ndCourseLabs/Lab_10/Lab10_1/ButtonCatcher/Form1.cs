6f552f39344201c6f9911a3df0c113f877bc001f
Brinckley <57827932+Brinckley@users.noreply.github.com>
2020-11-20 13:04:56 +0300

    release

---
+++ 2ndCourseLabs/Lab_10/Lab10_1/ButtonCatcher/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ButtonCatcher
+{
+    public partial class Form1 : Form
+    {
+        Random random;
+        public Form1()
+        {
+            InitializeComponent();
+            label1.Text = "";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "You've clicked the button!";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label1.Text = "WOW!";
+        }
+
+        private void button2_MouseMove(object sender, MouseEventArgs e)
+        {
+            random = new Random();
+
+            int x = button2.Location.X;
+            int y = button2.Location.Y;
+
+            int rx = random.Next(-10, 10);
+            int ry = random.Next(-10, 10);
+
+            bool flag = false;
+            do
+            {
+                if (x + rx > 0 && x + rx < this.ClientSize.Width - button2.Width/2 && y + ry < this.ClientSize.Height - button2.Height / 2 && y + ry > 0)
+                {
+                    button2.Location = new Point(x + rx, y + ry);
+                    flag = true;
+                }
+                else {
+                    rx = random.Next(-10, 10);
+                    ry = random.Next(-10, 10);
+                }
+            } while (flag == false);
+        }
+    }
+}
---
+++ 2ndCourseLabs/Lab_10/Lab10_2/PasswordShower/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PasswordShower
+{
+    public partial class Form1 : Form
+    {
+        string input;
+
+        public Form1()
+        {
+            InitializeComponent();
+            label1.Text = "";
+            input = "";
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Length == 1 && textBox1.Text != "*")
+            {
+                input += textBox1.Text;
+                label1.Text = input;
+                textBox1.Text = "*";
+            }
+            else if (textBox1.Text.Length == 2) {
+                input += textBox1.Text[0];
+                label1.Text = input;
+                textBox1.Text = "*";
+            }
+        }
+    }
+}
